1e4578d4feba0796a5e1cc64426f520ffae16789
Ajay Joshi <joshiajay311@gmail.com>
2023-05-16 18:33:18 +0530

    sidebar and dropdown added

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <template>
-  <div>
-    <h1 class="heading"
-    size="is-medium">Buefy Assignment</h1>
-    <Dropdown/>
-    <HelloWorld/>
-  </div>
+<div>
+    <h1 class="heading" size="is-medium">Buefy Assignment</h1>
+    <div>
+        <Sidebar />
+    </div>
+    <div class="drop">
+        <Dropdown />
+    </div>
+
+    <div class="Hello">
+        <HelloWorld />
+    </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 import Dropdown from './components/Dropdown.vue';
+import Sidebar from './components/Sidebar.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    Dropdown
-  }
+    name: 'App',
+    components: {
+        HelloWorld,
+        Dropdown,
+        Sidebar
+    }
 }
 </script>
 
+<style>
+.Hello {
+    margin-top: 15px;
+}
+</style>
--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,67 +1,59 @@
 <template>
 <div>
-    <template>
-        <section>
+    <section>
 
-            <b-dropdown aria-role="list">
-                <template #trigger="{ active }">
-                    <b-button label="Click me!" type="is-primary" :icon-right="active ? 'menu-up' : 'menu-down'" />
-                </template>
+        <b-dropdown aria-role="list">
+            <template #trigger="{ active }">
+                <b-button label="Click me!" type="is-primary" :icon-right="active ? 'menu-up' : 'menu-down'" />
+            </template>
 
-                <b-dropdown-item aria-role="listitem">Action</b-dropdown-item>
-                <b-dropdown-item aria-role="listitem">Another action</b-dropdown-item>
-                <b-dropdown-item aria-role="listitem">Something else</b-dropdown-item>
-            </b-dropdown>
+            <b-dropdown-item aria-role="listitem">Action</b-dropdown-item>
+            <b-dropdown-item aria-role="listitem">Another action</b-dropdown-item>
+            <b-dropdown-item aria-role="listitem">Something else</b-dropdown-item>
+        </b-dropdown>
 
-            <b-dropdown :triggers="['hover']" aria-role="list">
-                <template #trigger>
-                    <b-button label="Hover me!" type="is-info" icon-right="menu-down" />
-                </template>
+        <b-dropdown :triggers="['hover']" aria-role="list">
+            <template #trigger>
+                <b-button label="Hover me!" type="is-info" icon-right="menu-down" />
+            </template>
 
-                <b-dropdown-item aria-role="listitem">Action</b-dropdown-item>
-                <b-dropdown-item aria-role="listitem">Another action</b-dropdown-item>
-                <b-dropdown-item aria-role="listitem">Something else</b-dropdown-item>
-            </b-dropdown>
+            <b-dropdown-item aria-role="listitem">Action</b-dropdown-item>
+            <b-dropdown-item aria-role="listitem">Another action</b-dropdown-item>
+            <b-dropdown-item aria-role="listitem">Something else</b-dropdown-item>
+        </b-dropdown>
 
-            <b-dropdown disabled aria-role="list">
-                <template #trigger>
-                    <b-button label="Disabled" icon-right="menu-down" />
-                </template>
+        <b-dropdown disabled aria-role="list">
+            <template #trigger>
+                <b-button label="Disabled" icon-right="menu-down" />
+            </template>
 
-                <b-dropdown-item aria-role="listitem">Action</b-dropdown-item>
-                <b-dropdown-item aria-role="listitem">Another action</b-dropdown-item>
-                <b-dropdown-item aria-role="listitem">Something else</b-dropdown-item>
-            </b-dropdown>
+            <b-dropdown-item aria-role="listitem">Action</b-dropdown-item>
+            <b-dropdown-item aria-role="listitem">Another action</b-dropdown-item>
+            <b-dropdown-item aria-role="listitem">Something else</b-dropdown-item>
+        </b-dropdown>
 
-            <b-dropdown aria-role="list">
-                <template #trigger>
-                    <p class="tag is-success" role="button">
-                        Custom trigger
-                    </p>
-                </template>
+        <b-dropdown aria-role="list">
+            <template #trigger>
+                <p class="tag is-success" role="button">
+                    Custom trigger
+                </p>
+            </template>
 
-                <b-dropdown-item aria-role="listitem">Action</b-dropdown-item>
-                <b-dropdown-item aria-role="listitem">Another action</b-dropdown-item>
-                <b-dropdown-item aria-role="listitem">Something else</b-dropdown-item>
-            </b-dropdown>
+            <b-dropdown-item aria-role="listitem">Action</b-dropdown-item>
+            <b-dropdown-item aria-role="listitem">Another action</b-dropdown-item>
+            <b-dropdown-item aria-role="listitem">Something else</b-dropdown-item>
+        </b-dropdown>
 
-            <b-dropdown :triggers="['contextmenu']" aria-role="list">
-                <template #trigger>
-                    <b-button type="is-link" label="Right click" />
-                </template>
+        <b-dropdown :triggers="['contextmenu']" aria-role="list">
+            <template #trigger>
+                <b-button type="is-link" label="Right click" />
+            </template>
 
-                <b-dropdown-item aria-role="listitem">Action</b-dropdown-item>
-                <b-dropdown-item aria-role="listitem">Another action</b-dropdown-item>
-                <b-dropdown-item aria-role="listitem">Something else</b-dropdown-item>
-            </b-dropdown>
-        </section>
-    </template>
-
-    <style scoped>
-        .tag {
-            cursor: pointer;
-        }
-    </style>
+            <b-dropdown-item aria-role="listitem">Action</b-dropdown-item>
+            <b-dropdown-item aria-role="listitem">Another action</b-dropdown-item>
+            <b-dropdown-item aria-role="listitem">Something else</b-dropdown-item>
+        </b-dropdown>
+    </section>
 </div>
 </template>
 
@@ -71,3 +63,9 @@ export default ({
     name: 'drop-down',
 })
 </script>
+
+<style scoped>
+.tag {
+    cursor: pointer;
+}
+</style>
